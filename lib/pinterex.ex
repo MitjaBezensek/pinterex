9545e3d8a9947283b10173f9cc1af02e10c891b7
Mitja Bezenšek <mitja.bezensek@gmail.com>
2016-12-08 08:58:16 +0100

    Remove documentation for start function.

--- lib/pinterex.ex
+++ lib/pinterex.ex
@@ -1,19 +1,12 @@
 defmodule Pinterex do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
+  @doc false
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    # Define workers and child supervisors to be supervised
-    children = [
-      # Starts a worker by calling: Pinterex.Worker.start_link(arg1, arg2, arg3)
-      # worker(Pinterex.Worker, [arg1, arg2, arg3]),
-    ]
+    children = []
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Pinterex.Supervisor]
     Supervisor.start_link(children, opts)
   end
